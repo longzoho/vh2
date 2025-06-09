04d977ff5e97826d9f8f30884d0e9dc06c3c0cd8
Long Huynh 2 <long.huynh@siliconprime.com>
2025-06-09 17:35:13 +0700

    Update

--- src/components/ReasonsToBuy.jsx
+++ src/components/ReasonsToBuy.jsx
@@ -13,21 +13,21 @@ const ReasonsToBuy = () => {
         </Col>
         <Col span={24} style={{ textAlign: 'center' }}>
           <Row justify="center" gutter={[16, 16]} style={{ marginBottom: 40 }}>
-            <Col span={8} >
+            <Col xs={24} sm={12} md={8} lg={8} xl={8}>
               <Card style={{ backgroundColor: '#fff', height: '100%' }}>
                 <img src="reasons-to-buy/location.svg" alt="Vị trí thuận lợi, dễ dàng kết nối" style={{ width: '60px', height: '60px', color: '#FF6F61' }} />
                 <Title level={3} style={{ color: '#FF6F61' }}> Vị trí thuận lợi, dễ dàng kết nối</Title>
                 <p>Vinhomes Green City nằm ở vị trí trung tâm của huyện Đức Hòa, nơi có mạng lưới giao thông hoàn chỉnh dễ di chuyển đến các địa phương và khu vực khác. Đặc biệt, dự án gần với TP. HCM – trở thành lựa chọn lý tưởng để người dân Sài Thành chọn "an cư" dù "lập nghiệp" ở Sài Gòn.</p>
               </Card>
             </Col>
-            <Col span={8}>
+            <Col xs={24} sm={12} md={8} lg={8} xl={8}>
               <Card style={{ backgroundColor: '#fff', height: '100%' }}>
                 <img src="reasons-to-buy/home.svg" alt="Không gian sống TRỌN VẸN" style={{ width: '60px', height: '60px', color: '#FF6F61' }} />
                 <Title level={3} style={{ color: '#FF6F61' }}>Không gian sống TRỌN VẸN</Title>
                 <p>Mô hình "ALL IN ONE" được chủ đầu tư áp dụng triệt để tại Vinhomes Green City Long An. Nơi đây có đầy đủ mọi tiện ích phục vụ cho nhu cầu sống của con người, từ cơ bản cho đến cao cấp. Khi mọi thứ diễn ra trong một vài bước chân, không chỉ tiết kiệm thời gian cho cư dân mà còn nâng tầm chất lượng cuộc sống một cách ngoạn mục.</p>
               </Card>
             </Col>
-            <Col span={8}>
+            <Col xs={24} sm={12} md={8} lg={8} xl={8}>
               <Card style={{ backgroundColor: '#fff', height: '100%' }}>
                 <img src="reasons-to-buy/apartment.svg" alt="Đa dạng nhà ở để lựa chọn" style={{ width: '60px', height: '60px', color: '#FF6F61' }} />
                 <Title level={3} style={{ color: '#FF6F61' }}>Đa dạng nhà ở để lựa chọn</Title>
@@ -37,13 +37,13 @@ const ReasonsToBuy = () => {
 
           </Row>
           <Row gutter={[16, 16]} justify="center">
-            <Col span={8}>
+            <Col xs={24} sm={12} md={8} lg={8} xl={8}>
               <Card style={{ backgroundColor: '#fff', height: '100%' }}>
                 <img src="reasons-to-buy/law.svg" alt="Pháp lý vững vàng, chủ đầu tư uy tín" style={{ width: '60px', height: '60px', color: '#FF6F61' }} />
                 <Title level={3} style={{ color: '#FF6F61' }}>Pháp lý vững vàng, chủ đầu tư uy tín</Title>
                 <p>Vinhomes Green City đã được hoàn thiện về mặt hồ sơ pháp lý, đủ điều kiện để khởi công xây dựng và mở bán theo quy định. Công thêm sự uy tín của chủ đầu tư là Công ty CP Phát triển Thành Phố Xanh (thuộc Vinhomes) đem đến an tâm tuyệt đối cho khách hàng khi quyết định "xuống tiền" mua sản phẩm dự án.</p>
               </Card></Col>
-            <Col span={8}>
+            <Col xs={24} sm={12} md={8} lg={8} xl={8}>
               <Card style={{ backgroundColor: '#fff', height: '100%' }}>
                 <img src="reasons-to-buy/rise.svg" alt="Tiềm năng tăng giá cực lớn" style={{ width: '60px', height: '60px', color: '#FF6F61' }} />
                 <Title level={3} style={{ color: '#FF6F61' }}>Tiềm năng tăng giá cực lớn</Title>
